a6dcb8cc4fbeac42f454aead0ed3311b5552425f
iska1234 <sadhuisaac1@gmail.com>
2023-08-11 21:41:28 -0500

    update project
    updating

---
+++ client/App.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { Text, FlatList, SafeAreaView, StyleSheet } from "react-native";
+import InputTask from "./components/InputTask";
+import Task from "./components/Task";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const response = await fetch("http://192.168.1.40:8080/todos/1", {
+      headers: {
+        "x-api-key": "abcdef123456",
+      },
+    });
+    const data = await response.json();
+    setTodos(data);
+  }
+
+  function clearTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: todo.completed === 1 ? 0 : 1 }
+          : todo
+      )
+    );
+  }
+
+  return (
+    
+    <BottomSheetModalProvider>
+      <StatusBar />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={todos}
+          contentContainerStyle={styles.contentContainerStyle}
+          keyExtractor={(todo) => todo.id}
+          renderItem={({ item }) => (
+            <Task {...item} toggleTodo={toggleTodo} clearTodo={clearTodo} />
+          )}
+          ListHeaderComponent={() => <Text style={styles.title}>Today</Text>}
+        />
+        <InputTask todos={todos} setTodos={setTodos} />
+      </SafeAreaView>
+    </BottomSheetModalProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E9E9EF",
+  },
+  contentContainerStyle: {
+    padding: 15,
+  },
+  title: {
+    marginTop: 50,
+    fontWeight: "800",
+    fontSize: 28,
+    marginBottom: 15,
+  },
+});
